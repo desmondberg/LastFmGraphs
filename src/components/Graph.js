2f516dee2e9535f00bcaaf68f1360ce9ead16673
dessie <lynxes.desmond@gmail.com>
2024-10-16 11:43:47 +0100

    update readme, todo: restructure scrobbleData object and fetching method in order to get artist scrobbles as an array

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -28,7 +28,7 @@ export default function Graph(props) {
                 }
             ]
         }
-    )
+    );
     const [scrobbleData, setScrobbleData] = useState({
         dates: [
             {
@@ -44,7 +44,91 @@ export default function Graph(props) {
                 ]
             }
         ]
-    })
+    });
+
+    const [graphData, setGraphData] = useState(placeholders.placeholder_line);
+    const fetchDateRange = async (weeks, asArray) => {
+        //get weekly chart list - an object containing "chart", an array of all date ranges of user
+        await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getweeklychartlist&user=${props.username}&api_key=${LAST_FM_API_KEY}&format=json`)
+            .then((response) => response.json())
+            .then((json) => {
+                //reverse for easier indexing
+                let chartList = json.weeklychartlist.chart.reverse();
+                //if asArray is true, insert each date range as an element of the "weeks" array
+                if (asArray) {
+                    chartList = chartList.slice(0, weeks).reverse();
+                    const formattedWeeks = chartList.map((chart, index) => ({
+                        week: index,
+                        from: chart.from,
+                        to: chart.to,
+                    }));
+                    console.log(formattedWeeks)
+                    setScrobbleQuery({
+                        //username stays the same
+                        ...scrobbleQuery,
+                        dates: formattedWeeks
+                    })
+                    console.log(scrobbleQuery);
+
+                }
+                //if asArray is false, only take the "from" of the earliest date range and the "to" of the latest date range
+                else {
+                    setScrobbleQuery({
+                        //username stays the same
+                        ...scrobbleQuery,
+                        dates: [
+                            {
+                                week: 0,
+                                from: chartList[weeks - 1].from,
+                                to: chartList[0].to
+                            }
+                        ]
+                    });
+                }
+            })
+            .catch((e) => { setTestString(e.toString()) })
+
+    }
+    const fetchScrobbles = async (query) => {
+
+        for (let dateRange in query.dates) {
+            await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user=${query.user}&from=${query.dates[dateRange].from}&to=${query.dates[dateRange].to}&api_key=${LAST_FM_API_KEY}&format=json`)
+                .then((response) => response.json())
+                .then((json) => {
+                    let topArtists = json.weeklyartistchart.artist.slice(0, 3);
+                    //setTestString(JSON.stringify(topArtists));
+                    //convert unix timestamps into readable date
+                    let startingWeek = [
+                        timestamp.toDate(parseInt((query.dates[dateRange].from))).toString().split(" ")[1],
+                        timestamp.toDate(parseInt((query.dates[dateRange].from))).toString().split(" ")[2],
+                        timestamp.toDate(parseInt((query.dates[dateRange].from))).toString().split(" ")[3],
+                    ].join(" ");
+                    console.log(startingWeek);
+
+                    //format query result into custom object
+                    const formattedArtistChart = {
+                        dateRange: dateRange,
+                        startingWeek: startingWeek,
+                        artists: topArtists.map((artist) => ({
+                            rank: artist["@attr"].rank,
+                            artist: artist.name,
+                            scrobbles: artist.playcount,
+                            url: artist.url
+                        }))
+                    };
+                    //keep previous results and append the current result
+                    setScrobbleData((previous) => ({
+                        ...previous,
+                        dates: [...previous.dates, formattedArtistChart]
+                    }));
+                    console.log("scrobble data fetch successful");
+                })
+                .catch((e) => {
+                    console.error(e);  // Handle any fetch errors
+                });
+
+        }
+    }
 
     //fetching last.fm API
 
@@ -72,101 +156,19 @@ export default function Graph(props) {
                         return 1;
                 }
             }
-            const fetchDateRange = async (weeks, asArray) => {
-                //get weekly chart list - an object containing "chart", an array of all date ranges of user
-                await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getweeklychartlist&user=${props.username}&api_key=${LAST_FM_API_KEY}&format=json`)
-                    .then((response) => response.json())
-                    .then((json) => {
-                        //reverse for easier indexing
-                        let chartList = json.weeklychartlist.chart.reverse();
-                        //if asArray is true, insert each date range as an element of the "weeks" array
-                        if (asArray) {
-                            chartList = chartList.slice(0, weeks).reverse();
-                            const formattedWeeks = chartList.map((chart, index) => ({
-                                week: index,
-                                from: chart.from,
-                                to: chart.to,
-                            }));
-                            console.log(formattedWeeks)
-                            setScrobbleQuery({
-                                //username stays the same
-                                ...scrobbleQuery,
-                                dates: formattedWeeks
-                            })
-                            console.log(scrobbleQuery);
 
-                        }
-                        //if asArray is false, only take the "from" of the earliest date range and the "to" of the latest date range
-                        else {
-                            setScrobbleQuery({
-                                //username stays the same
-                                ...scrobbleQuery,
-                                dates: [
-                                    {
-                                        week: 0,
-                                        from: chartList[weeks - 1].from,
-                                        to: chartList[0].to
-                                    }
-                                ]
-                            });
-                        }
-                    })
-                    .catch((e) => { setTestString(e.toString()) })
-
-            }
-
-            const fetchScrobbles = async (query) => {
-
-                for (let dateRange in query.dates) {
-                    await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getweeklyartistchart&user=${query.user}&from=${query.dates[dateRange].from}&to=${query.dates[dateRange].to}&api_key=${LAST_FM_API_KEY}&format=json`)
-                        .then((response) => response.json())
-                        .then((json) => {
-                            let topArtists = json.weeklyartistchart.artist.slice(0, 3);
-                            //setTestString(JSON.stringify(topArtists));
-                            //convert unix timestamps into readable date
-                            let startingWeek = [
-                                timestamp.toDate(parseInt((query.dates[dateRange].from))).toString().split(" ")[1],
-                                timestamp.toDate(parseInt((query.dates[dateRange].from))).toString().split(" ")[2],
-                                timestamp.toDate(parseInt((query.dates[dateRange].from))).toString().split(" ")[3],
-                            ].join(" ");
-                            const formattedArtistChart = {
-                                dateRange: dateRange,
-                                startingWeek:startingWeek,
-                                artists: topArtists.map((artist) => ({
-                                    rank: artist["@attr"].rank,
-                                    artist: artist.name,
-                                    scrobbles: artist.playcount,
-                                    url: artist.url
-                                }))
-                            };
-
-                            setScrobbleData((previous) => ({
-                                ...previous,
-                                dates: [...previous.dates, formattedArtistChart]
-                            }));
-                            setTestString(JSON.stringify(scrobbleData));
-                            console.log("scrobble data fetch successful");
-                        })
-                        .catch((e) => {
-                            console.error(e);  // Handle any fetch errors
-                        });
-
-                }
-            }
-
-
-
-            (async () => {
-                const asArray = true;
-                await fetchDateRange(getTimeframe(), asArray);
-                await fetchScrobbles(scrobbleQuery);
-            })();
-
-
-
-
+            fetchDateRange(getTimeframe(), true);
         }
     }, [props.buttonPressed])
+
+    useEffect(() => {
+        fetchScrobbles(scrobbleQuery);
+    }, [scrobbleQuery])
+
+    useEffect(() => {
+        setTestString(JSON.stringify(scrobbleData));
+    }, [scrobbleData])
+    
 
 
     return (
@@ -179,7 +181,7 @@ export default function Graph(props) {
 
                         <div className="graph">
                             {/* conditional rendering based on selected chart type */}
-                            {props.chartType === "line" && <Line data={placeholders.placeholder_line.data} />}
+                            {props.chartType === "line" && <Line data={graphData.data} />}
                             {props.chartType === "bar" && <Bar data={placeholders.placeholder_bar.data} />}
                             {props.chartType === "donut" && <Doughnut data={placeholders.placeholder_doughnut.data} />}
                         </div>
